62b9f3072c816fbf7ec1d7ac88797ac72a7adcf6
10takla <letifovabakar98@gmail.com>
2024-01-22 23:58:09 +0300

    update gitignore

--- game_of_life/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import {createRoot} from "react-dom/client";
-import {App} from "./app/App";
-
-const element = document.getElementById("root");
-if (element) {
-    const root = createRoot(element)
-    root.render(<App/>)
-}
-
